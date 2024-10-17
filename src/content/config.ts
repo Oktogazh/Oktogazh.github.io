58d94b0a2adbab235fbd60a21d8d1787c5e14793
Alan Kersaudy <alan.kersaudy@gmail.com>
2024-10-17 13:16:58 +0100

    add: the minutes of the meetings in the project pages

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,4 @@
-import { defineCollection, z } from 'astro:content'
+import { defineCollection, reference, z } from 'astro:content'
 
 export const collections = {
   projects: defineCollection({
@@ -7,6 +7,17 @@ export const collections = {
       title: z.string(),
       description: z.string(),
       id: z.number(),
+    }).strict(),
+  }),
+  meetings: defineCollection({
+    type: 'content',
+    schema: z.object({
+      title: z.string(),
+      date: z.string().transform((str) => {
+        str.split('/').reverse().join('-')
+        return new Date(str)
+      }),
+      project: reference('projects'),
     }).strict(),
   })
 };
