a6ba8288bc5d5d5d51eb3862e33cd9a7b96d84a3
Alan Kersaudy <alan.kersaudy@gmail.com>
2024-12-18 18:53:15 +0100

    update: content

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,6 +12,7 @@ export default defineConfig({
   base: "/",
   integrations: [
     mdx({
+      syntaxHighlight: "prism",
       remarkPlugins: [remarkMath],
       rehypePlugins: [rehypeKatex],
     }),
